afa943b1eb62a0cef1fb80183d3aeede3468a644
Meitar Moscovitz <meitarm@gmail.com>
2013-03-07 01:34:49 -0700

    Disable logging errors by default. Users can uncomment if they wish.

--- FetLife.php
+++ FetLife.php
@@ -24,8 +24,8 @@
  */
 
 // Uncomment for minimal debugging.
-ini_set('log_errors', true);
-ini_set('error_log', '/tmp/php_errors.log');
+//ini_set('log_errors', true);
+//ini_set('error_log', '/tmp/php_errors.log');
 
 /**
  * Base class.
